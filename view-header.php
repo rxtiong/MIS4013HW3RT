22b4145644aca9b3c8bb1916957f5fda4fde4ade
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-01 14:11:02 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,13 +20,13 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="customers.php">Customers</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post Result</a>
+          <a class="nav-link" href="orders.php">Orders</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
+          <a class="nav-link" href="products.php">Products</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="get-result.php">Get Result</a>
