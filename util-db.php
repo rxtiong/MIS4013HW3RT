fd0d41dfcaae8f1429fe5c24218366d81167a92e
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 13:40:36 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -2,7 +2,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('mis4013hw3rtdb.mysql.database.azure.com', 'rxtiong', 'Rac86952', 'storedb');
+    $conn = new mysqli('mis4013hw3rtdb.mysql.database.azure.com', 'rxtiong', '@Rac86952', 'storedb');
     
     // Check connection
     if ($conn->connect_error) {
