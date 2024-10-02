b46ad1780481713fc5d8f8b2f33d52ef78f86b3a
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 13:38:17 -0500

    Update model-customers.php

--- model-customers.php
+++ model-customers.php
@@ -1,16 +1,11 @@
 <?php
 function selectCustomers() {
-    try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select customer_id, first_name, last_name, dob, email, phone, order_id from customers;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
 }
 
 ?>
