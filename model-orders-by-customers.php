41dd356bb77356c2acad81967e8011bd5c4e8ee6
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 17:22:49 -0500

    Update model-orders-by-customers.php

--- model-orders-by-customers.php
+++ model-orders-by-customers.php
@@ -2,7 +2,7 @@
 function selectOrdersByCustomers($cid) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select o.order_id, o.customer_id, first_name, last_name, order_date, total_amount 
-                                from orders o join customers c on o.order_id=c.order_id where c.customer_id=?");
+                                from orders o join customers c on o.order_id=c.order_id where c.order_id=?");
         $stmt->bind_param("i", $cid);
         $stmt->execute();
         $result = $stmt->get_result();
