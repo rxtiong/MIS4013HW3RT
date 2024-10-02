1ff6e682950e0214bd3d218369451fc092c4d114
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 15:46:19 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-orders.php");
 $pageTitle = "Orders";
 include "view-header.php";
-$customers = selectOrders();
+$orders = selectOrders();
 include "view-orders.php";
 include "view-footer.php";
 ?>
