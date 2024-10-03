4c834d0214cee734b010bae53c421302e17cdfa6
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 20:14:01 -0500

    Create view-orders-by-employees.php

---
+++ view-orders-by-employees.php
@@ -0,0 +1,31 @@
+<h1>Orders by employees</h1>
+
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Order ID</th>
+      <th>Employee ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th></th>
+      </tr>      
+    </thead>
+    <tbody>
+<?php
+while ($employee = $employees->fetch_assoc())
+{
+  ?>
+  <tr>
+    <td><?php echo $employee['order_id'];?></td>
+    <td><?php echo $employee['employee_id'];?></td>
+    <td><?php echo $employee['first_name'];?></td>
+    <td><?php echo $employee['last_name'];?></td>
+  </tr>
+  <?php
+}
+?>
+      
+    </tbody>
+  </table>
+</div>
