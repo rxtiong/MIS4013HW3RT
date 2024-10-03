dd01c724c7e792d2aa955f730b0c8846aaf9dcdc
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 20:46:08 -0500

    Update orders-by-employees.php

--- orders-by-employees.php
+++ orders-by-employees.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-orders-by-employees.php");
 $pageTitle = "Orders by customers";
 include "view-header.php";
-$employees = selectOrdersByEmployees($_GET['id']);
+$employees = selectOrdersByEmployees($_POST['id']);
 include "view-orders-by-employees.php";
 include "view-footer.php";
 ?>
