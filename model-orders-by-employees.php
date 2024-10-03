e0b226fac04c235468ed323bce83815ae8542e3f
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 20:42:46 -0500

    Update model-orders-by-employees.php

--- model-orders-by-employees.php
+++ model-orders-by-employees.php
@@ -1,7 +1,7 @@
 <?php
 function selectOrdersByEmployees($eid) {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select o.order_id, o.employee_id, first_name, last_name from orders o join employees e on o.employee_id=e.employee_id where o.employee_id=5001;");
+        $stmt = $conn->prepare("select o.order_id, o.employee_id, first_name, last_name from orders o join employees e on o.employee_id=e.employee_id where o.employee_id=?;");
         $stmt->bind_param("i", $eid);
         $stmt->execute();
         $result = $stmt->get_result();
