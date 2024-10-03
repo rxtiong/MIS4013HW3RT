eeaa61debecf07c964a57f6b41f982a003cfc578
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-02 20:07:19 -0500

    Update model-employees.php

--- model-employees.php
+++ model-employees.php
@@ -1,7 +1,7 @@
 <?php
 function selectEmployees() {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select employee_id, order_id, first_name, last_name from employees");
+        $stmt = $conn->prepare("select employee_id, first_name, last_name from employees");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
