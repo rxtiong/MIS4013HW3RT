bd8d8d1bcf0800d9cddee9d07c554a9692991e85
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-25 15:14:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework 3</h1>
+    <h1>Homework 4</h1>
     <p>This homework shows data from a database. Customers have one order and employees have one order, but an order 
     can have multiple customers and/or employees. Orders contain one product, but products can be in multiple orders.</p>
     
