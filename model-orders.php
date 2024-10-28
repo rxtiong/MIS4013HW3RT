65372c3f2ef5d8399bc2356893394f39a1f96639
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-28 10:56:45 -0500

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -1,5 +1,5 @@
 <?php
-function selectOrders() {
+function selectOrder() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, product_id from orders;");
         $stmt->execute();
@@ -13,6 +13,25 @@ function insertOrder($oCust, $oDate, $oAmount, $oProd) {
         $stmt = $conn->prepare("insert into 'orders' ('customer_id','order_date','total_amount','product_id')
                                 values (?,?,?,?)");
         $stmt->bind_param("ssss", $oCust, $oDate, $oAmount, $oProd);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function updateOrder($oCust, $oDate, $oAmount, $oProd, $oid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update orders set 'customer_id' = ?,'order_date'=?,'total_amount'=?,'product_id'=?
+                                where order_id = ?");
+        $stmt->bind_param("ssssi", $oCust, $oDate, $oAmount, $oProd, $oid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function deleteOrder($oid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from order where order_id=?");
+        $stmt->bind_param("i", $oid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
