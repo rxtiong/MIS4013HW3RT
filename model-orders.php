23309b441fb62d46dd8f63dcd3a07bbea1e11e00
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-28 13:36:56 -0500

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -1,5 +1,5 @@
 <?php
-function selectOrder() {
+function selectOrders() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, product_id from orders;");
         $stmt->execute();
