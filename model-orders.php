b6e9b9a32d7a6d2a5e3c29c4991495eacaef7b92
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-29 00:07:28 -0500

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -1,28 +1,28 @@
 <?php
 function selectOrders() {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, product_id from orders;");
+        $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, product_id, employee_id from orders;");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
         return $result;
 }
 
-function insertOrder($oCust, $oDate, $oAmount, $oProd) {
+function insertOrder($oCust, $oDate, $oAmount, $oProd, $eid) {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("insert into orders ('customer_id','order_date','total_amount','product_id')
+        $stmt = $conn->prepare("insert into orders ('customer_id','order_date','total_amount','product_id','employee_id')
                                 values (?,?,?,?)");
-        $stmt->bind_param("ssss", $oCust, $oDate, $oAmount, $oProd);
+        $stmt->bind_param("ssss", $oCust, $oDate, $oAmount, $oProd, $eid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
 }
 
-function updateOrder($oCust, $oDate, $oAmount, $oProd, $oid) {
+function updateOrder($oCust, $oDate, $oAmount, $oProd, #eid, $oid) {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("update orders set 'customer_id' = ?,'order_date'=?,'total_amount'=?,'product_id'=?
+        $stmt = $conn->prepare("update orders set 'customer_id' = ?,'order_date'=?,'total_amount'=?,'product_id'=?, 'employee_id'=?
                                 where order_id = ?");
-        $stmt->bind_param("ssssi", $oCust, $oDate, $oAmount, $oProd, $oid);
+        $stmt->bind_param("sssssi", $oCust, $oDate, $oAmount, $oProd, $eid, $oid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
