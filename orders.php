cc8381b817e52c88a1b6c939de3b1ff77925914e
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-28 23:52:07 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,6 +3,15 @@ require_once("util-db.php");
 require_once("model-orders.php");
 $pageTitle = "Orders";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertOrder($_POST['cNumber'], $_POST['oDate'], $_POST['oTotal'], $_POST['oProd']);
+    break;
+  }
+}
+
 $orders = selectOrders();
 include "view-orders.php";
 include "view-footer.php";
