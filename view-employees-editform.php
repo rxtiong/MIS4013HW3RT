dd650186f67637dd3acbb17fd08e22af82747bcd
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 16:27:29 -0600

    Update view-employees-editform.php

--- view-employees-editform.php
+++ view-employees-editform.php
@@ -23,7 +23,7 @@
             </div>
           
             <div class="mb-3">
-              <label for="eLName<?php echo $employee['employee_name']; ?>" class="form-label">Last Name</label>
+              <label for="eLName<?php echo $employee['last_name']; ?>" class="form-label">Last Name</label>
               <input type="text" class="form-control" id="eLName" name="eLName" value="<?php echo $employee['last_name']; ?>">
             </div>            
             
