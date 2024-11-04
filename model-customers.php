594998a9ed30e5fc8671442d178c25fa5042ec32
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 13:19:37 -0600

    Update model-customers.php

--- model-customers.php
+++ model-customers.php
@@ -8,4 +8,32 @@ function selectCustomers() {
         return $result;
 }
 
+function inserCustomer($cFName, $cLName, $cdob, $cEmail, $cPhone, $cOrder) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("insert into customers (first_name, last_name, dob, email, phone, order_id)
+                                values (?,?,?,?,?,?);");
+        $stmt->bind_param("ssssss", $cFName, $cLName, $cdob, $cEmail, $cPhone, $cOrder);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function updateCustomer($cFName, $cLName, $cdob, $cEmail, $cPhone, $cOrder, $cid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update customers set first_name = ?,last_name=?,dob=?,email=?, phone=?, order_id=?
+                                where customer_id = ?;");
+        $stmt->bind_param("ssssssi", $cFName, $cLName, $cdob, $cEmail, $cPhone, $cOrder, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function deleteCustomer($cid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from customers where customer_id=?;");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
 ?>
