297651064524351c12461258d54b2ae5c55d7e03
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 16:14:48 -0600

    Update model-employees.php

--- model-employees.php
+++ model-employees.php
@@ -8,4 +8,34 @@ function selectEmployees() {
         return $result;
 }
 
+function insertEmployee($eFName, $eLName) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("insert into employee (first_name, last_name)
+                                values (?,?);");
+        $stmt->bind_param("ss", $eFName, $eLName);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function updateEmployee($eFName, $eLName, $eid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update employees set first_name =?, last_name=?
+                                where employee_id = ?;");
+        $stmt->bind_param("ssi", $eFName, $eLName, $eid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+function deleteEmployee($eid) {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from employees where employee_id=?;");
+        $stmt->bind_param("i", $eid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+}
+
+
 ?>
