57c9df8225d10f24068579b177cb70b2e463a021
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 16:21:13 -0600

    Create view-employees-editform.php

---
+++ view-employees-editform.php
@@ -0,0 +1,37 @@
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editEmployeeModal<?php echo $employee['employee_id']; ?>">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+</svg>
+
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="editEmployeeModal<?php echo $employee['employee_id']; ?>" tabindex="-1" aria-labelledby="editEmployeeModalLabel<?php echo $employee['employee_id']; ?>" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="editEmployeeModalLabel<?php echo $employee['employee_id']; ?>">Edit Employee</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+            <div class="mb-3">
+              <label for="eFName<?php echo $employee['first_name']; ?>" class="form-label">First Name</label>
+              <input type="text" class="form-control" id="eFName" name='eFName' value="<?php echo $employee['first_name']; ?>">
+            </div>
+          
+            <div class="mb-3">
+              <label for="eLName<?php echo $employee['employee_name']; ?>" class="form-label">Last Name</label>
+              <input type="text" class="form-control" id="eLName" name="eLName" value="<?php echo $employee['last_name']; ?>">
+            </div>            
+            
+            <input type="hidden" name="eid" value="<?php echo $employee['employee_id']; ?>">
+            <input type="hidden" name="actionType" value="Edit">
+            <button type="submit" class="btn btn-primary">Save</button>
+      </form>
+      </div>      
+    </div>
+  </div>
+</div>
