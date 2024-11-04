43543def08da3a8ecfdebf1e96f94356f148537a
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 16:08:38 -0600

    Update employees.php

--- employees.php
+++ employees.php
@@ -3,6 +3,35 @@ require_once("util-db.php");
 require_once("model-employees.php");
 $pageTitle = "Employees";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertEmployee($_POST['eFName'], $_POST['eLName']))
+      { echo '<div class="alert alert-success" role="alert">Employee Added!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+
+    case "Edit":
+      if (updateEmployee($_POST['eFName'], $_POST['eLName'],$_POST['eid']))
+      { echo '<div class="alert alert-success" role="alert">Employee Edited!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+    
+    case "Delete":
+      if (deleteEmployee($_POST['eid']))
+      { echo '<div class="alert alert-success" role="alert">Employee Deleted!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+  }
+}
+
 $employees = selectEmployees();
 include "view-employees.php";
 include "view-footer.php";
