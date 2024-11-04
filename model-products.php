8b94a4005e9c3301c653f33bc6da6d43addefd88
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-04 11:55:09 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -8,15 +8,6 @@ function selectProducts() {
         return $result;
 }
 
-function selectOrders() {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, product_id, employee_id from orders;");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-}
-
 function insertProduct($pOrder, $pName, $pCategory, $pPrice) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("insert into products (order_id, product_name, category, price)
