c7428e4fa209cba2346307602ecdaaf89cdf1a19
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-05 14:36:19 -0600

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -7,7 +7,26 @@ function selectOrders() {
         $conn->close();
         return $result;
 }
+#startign changes for drop down
+function selectProductsForInput() {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select product_id, product_name from products order by product_name;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+}
 
+function selectEmployeesForInput() {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select employee_id, first_name, last_name from employees order by last_name;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+}
+
+#end changes for dropdown
 function insertOrder($eid, $oCust, $oDate, $oAmount, $oProd) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("insert into orders (employee_id, customer_id, order_date, total_amount, product_id)
