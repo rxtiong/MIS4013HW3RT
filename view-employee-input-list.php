81acfd15aa3bda37f8bf27443afaa41e167f22c2
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-05 23:40:40 -0600

    Update view-employee-input-list.php

--- view-employee-input-list.php
+++ view-employee-input-list.php
@@ -2,8 +2,12 @@
   <?php
     while ($employeeItem = $employeeList -> fetch_assoc())
     {
+      $selText = "";
+      if ($selectedEmployee == $employeeItem['employee_id']) {
+          $selText = " selected";
+      }
        ?>
-      <option value="<?php echo $employeeItem['employee_id']; ?>">
+      <option value="<?php echo $employeeItem['employee_id']; ?>"<?=$selText?>>
         <?php echo $employeeItem['first_name']; ?>
         <?php echo $employeeItem['last_name']; ?>
       </option>
