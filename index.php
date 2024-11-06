53eb06844f50901bb21f736a58c732d939d9bb9c
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-05 23:46:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ include "view-header.php";
     <h1>Homework 4</h1>
     <p>This homework shows data from a database. Customers have one order and employees have one order, but an order 
     can have multiple customers and/or employees. Orders contain one product, but products can be in multiple orders.</p>
+
+    <p>
+        The drop down list is under Orders.
+    </p>
     
 <?php 
 include "view-footer.php";
