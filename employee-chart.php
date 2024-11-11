5b0ec6f76d213977936a8bdac374baf66b4718a4
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-11 12:30:21 -0600

    Create employee-chart.php

---
+++ employee-chart.php
@@ -0,0 +1,38 @@
+<?php 
+require_once("util-db.php");
+require_once("model-employees-chart.php");
+$pageTitle = "Employees Chart";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertEmployee($_POST['eFName'], $_POST['eLName']))
+      { echo '<div class="alert alert-success" role="alert">Employee Added!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+
+    case "Edit":
+      if (updateEmployee($_POST['eFName'], $_POST['eLName'],$_POST['eid']))
+      { echo '<div class="alert alert-success" role="alert">Employee Edited!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+    
+    case "Delete":
+      if (deleteEmployee($_POST['eid']))
+      { echo '<div class="alert alert-success" role="alert">Employee Deleted!</div>';}
+      else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }      
+    break;
+  }
+}
+
+$employees = selectEmployees();
+include "view-employees-chart.php";
+include "view-footer.php";
+?>
