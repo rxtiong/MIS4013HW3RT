8ea21b47b3851491e1a55e8e44b8e773f47dd487
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-11 13:15:53 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -34,6 +34,9 @@
         <li class="nav-item">
           <a class="nav-link" href="employees-with-orders.php">Employees with orders</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="employee-chart.php">Employee Chart</a>
+        </li>
       </ul>
     </div>
   </div>
