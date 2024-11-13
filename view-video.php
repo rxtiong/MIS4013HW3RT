eb55f9cc3a226882a4b4d06bbdd238297ee718eb
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-13 13:50:14 -0600

    Update view-video.php

--- view-video.php
+++ view-video.php
@@ -5,6 +5,18 @@
   </div>
 
   // add video script here???
+  <body>
+  <iframe
+    id="my-video"
+    width="640"
+    height="360"
+    src="https://www.youtube.com/watch?v=0jIeCAOkgcQ"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</body>
   
   <div class="col-auto">
     <?php
