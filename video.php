28dea751eea2b99daa267ece24e308cbfce1ea87
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-13 13:44:35 -0600

    Create video.php

---
+++ video.php
@@ -0,0 +1,10 @@
+<?php 
+require_once("util-db.php");
+require_once("model-video.php");
+$pageTitle = "Coffee Video";
+include "view-header.php";
+
+// $products = selectProducts();
+include "view-video.php";
+include "view-footer.php";
+?>
