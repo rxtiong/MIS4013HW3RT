1675bb1da69189c4effbc008a408ac8f4b25cc09
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-13 13:12:14 -0600

    Update employee-chart.php

--- employee-chart.php
+++ employee-chart.php
@@ -4,33 +4,7 @@ require_once("model-employee-chart-db.php");
 $pageTitle = "Employees Chart";
 include "view-header.php";
 
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if (insertEmployee($_POST['eFName'], $_POST['eLName']))
-      { echo '<div class="alert alert-success" role="alert">Employee Added!</div>';}
-      else {
-        echo '<div class="alert alert-danger" role="alert">Error</div>';
-      }      
-    break;
 
-    case "Edit":
-      if (updateEmployee($_POST['eFName'], $_POST['eLName'],$_POST['eid']))
-      { echo '<div class="alert alert-success" role="alert">Employee Edited!</div>';}
-      else {
-        echo '<div class="alert alert-danger" role="alert">Error</div>';
-      }      
-    break;
-    
-    case "Delete":
-      if (deleteEmployee($_POST['eid']))
-      { echo '<div class="alert alert-success" role="alert">Employee Deleted!</div>';}
-      else {
-        echo '<div class="alert alert-danger" role="alert">Error</div>';
-      }      
-    break;
-  }
-}
 
 $employees = selectEmployees();
 include "view-employees-chart.php";
