1ace6079a1f1907ef0a08928126a1657161960c3
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-13 13:47:57 -0600

    Create view-video.php

---
+++ view-video.php
@@ -0,0 +1,16 @@
+<div class="row">
+
+  <div class="col">
+    <h1>Coffee Video</h1>
+  </div>
+
+  // add video script here???
+  
+  <div class="col-auto">
+    <?php
+    include "view-customers-newform.php";
+    ?>
+  </div>
+</div>
+
+
