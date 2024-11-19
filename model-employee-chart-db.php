b0c7b49abb62b3c6694596460056096739a1011b
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-18 18:15:40 -0600

    Create model-employee-chart-db.php

---
+++ model-employee-chart-db.php
@@ -0,0 +1,17 @@
+<?php
+function selectEmployees() {
+       try{ $conn = get_db_connection();
+        $stmt = $conn->prepare("select first_name, last_name, count(order_id) as NumOfOrders
+                                from employees e join 
+                                orders o on e.employee_id=o.employee_id
+                                group by e.employee_id;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+} catch (Exception $e) {
+  $conn -> close(); 
+  throw $e;
+}
+}
+?>
