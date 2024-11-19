009608f112d198bc48941df2f253892419de591e
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-19 13:59:10 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <div class="container">
+    <div class="container" style="border-radius: 25%">
       <nav class="navbar navbar-expand-lg" style="background-color: #c4a484; font-color: #ffffff;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Coffee Shop</a>
