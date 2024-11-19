4a0ea67f4ab568533ffbd5f35b3df675ed51c1b7
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-19 16:06:03 -0600

    Update view-employee-chart.php

--- view-employee-chart.php
+++ view-employee-chart.php
@@ -1,7 +1,7 @@
 <h1>Employee Chart</h1>
 
 <div>
-  <canvas id="myChart"></canvas>
+  <canvas id="myChart" width="500" height="400" ></canvas>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
