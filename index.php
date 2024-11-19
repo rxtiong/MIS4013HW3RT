87719f9ee3567ce3408669b9a7a71b99b9373336
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-19 13:57:34 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
     <h1>Coffee Shop</h1>
-<img src="coffee.jgp" style="height:300px;" />
+<img src="https://i.pinimg.com/originals/0f/ad/28/0fad289870489c693936a394112b07f2.jpg" style="height:300px;" />
     <p>This webpage shows customer data, order information, product information, and employee information and data. Customers have one order and employees have one order, but an order 
     can have multiple customers and/or employees. Orders contain one product, but products can be in multiple orders.</p>
 
