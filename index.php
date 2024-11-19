ca8482c1f7d123c79e2a9ae2f446fdf687d2eb98
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-18 18:20:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-    <h1>Homework 4</h1>
-    <p>This homework shows data from a database. Customers have one order and employees have one order, but an order 
+    <h1>Coffee Shop Statistics</h1>
+    <p>This webpage shows customer data, order information, product information, and employee information and data. Customers have one order and employees have one order, but an order 
     can have multiple customers and/or employees. Orders contain one product, but products can be in multiple orders.</p>
 
     <p>
-        The drop down list is under Orders.
+        Each page is customizable for the user. They can add, edit, or delete any information on the webpage. 
     </p>
     
 <?php 
