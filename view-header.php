b5dd9ce73e28bc800d531798b8f4ff8f1de04f17
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-19 13:52:16 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -6,7 +6,7 @@
     <title><?=$pageTitle?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
+  <body style="background-color:tan">
     <div class="container">
       <nav class="navbar navbar-expand-lg" style="background-color: #c4a484; font-color: #ffffff;">
   <div class="container-fluid">
