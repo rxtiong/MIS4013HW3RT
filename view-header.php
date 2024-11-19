7ee5e1deb8a45fdfb3b3d7455e8ea81d98d5f9a1
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-19 16:15:01 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -29,6 +29,7 @@
 
 <link href="https://juxinglong.github.io/static/MIS3033.css" rel="stylesheet" />
 
+<link rel="icon" href="https://cdn-icons-png.flaticon.com/512/36/36578.png"/>    
   </head>
   <body>
     <div class="container">
