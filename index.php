65ec75ec9cf9e1d08f6ee5f0b9322cbcccc7652a
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-11-21 17:50:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php 
 $pageTitle = "Home";
 include "view-header.php";
+<head>
 <script>
 function myFunction() {
   alert("Page is loaded");
 }
 </script>
+  </head>
 ?>
 
 <div><h1>Coffee Shop</h1></div>
